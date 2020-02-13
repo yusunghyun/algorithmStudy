1e2440b6f5335e32c283f364b80bb9e8653e7c7d
SungHyun Yu <ysh1513@naver.com>
2020-02-13 23:01:31 +0900

    test 해야댄당!

---
+++ 01. stack/YSH/test.test.js
@@ -0,0 +1,25 @@
+import Stack from './stack'
+import Queue from './queue'
+//npm run test
+
+test('stack', () => {
+  expect(stackSolution()).toEqual(1);
+  expect(stackSolution()).toEqual(1);
+  expect(stackSolution()).toEqual(1);
+  expect(stackSolution()).toEqual(1);
+});
+
+function stackSolution(){
+
+}
+
+test('queue', () => {
+  expect(queueStack()).toEqual(1);
+  expect(queueStack()).toEqual(1);
+  expect(queueStack()).toEqual(1);
+  expect(queueStack()).toEqual(1);
+});
+
+function queueStack(){
+
+}
